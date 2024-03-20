a7c1b73fba081d3d17a6a4496d0c639ac4f1b3c1
cameron wood <cwood14@huskers.unl.edu>
2024-03-20 10:34:23 -0500

    Logic working

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ class Item {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.total = price * quantity;
     }
 }
 
-let itemList = [];
+const itemTable = document.getElementById("itemTable");
 
-let itemTable = document.getElementById("itemTable");
+const totalQuantityCell = document.getElementById('totalQuantity');
+const totalPriceCell = document.getElementById('totalPrice');
 
 const deleteBtnHTML = `<td><button class="delete-btn">X</button></td>`
 
@@ -16,16 +18,53 @@ let newItemForm = document.getElementById("newItemForm");
 newItemForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const item = new Item("Steak", 9.99, 2);
-    itemList[0] = item;
+    let itemName = document.getElementById('name').value
+    let itemPrice = document.getElementById('price').value
+    let itemQty = document.getElementById('quantity').value
 
-    var row = itemTable.insertRow(2);
+    const item = new Item(itemName, itemPrice, itemQty);
+
+    var row = itemTable.insertRow(1);
     var nameCell = row.insertCell(0);
     var priceCell = row.insertCell(1);
     var qtyCell = row.insertCell(2);
-    var deleteBtn = row.insertCell(3);
+    var totalCell = row.insertCell(3);
+    var deleteBtn = row.insertCell(4);
     nameCell.innerHTML = item.name;
     priceCell.innerHTML = "$" + item.price;
     qtyCell.innerHTML = item.quantity;
+    totalCell.innerHTML = "$" + item.total;
     deleteBtn.innerHTML = deleteBtnHTML;
+
+    getTotals();
 });
+
+function deleteRow(event) {
+    const row = event.target.closest('tr');
+    row.remove();
+
+    getTotals();
+}
+
+itemTable.addEventListener('click', function(event) {
+    if(event.target.matches('.delete-btn')) {
+        deleteRow(event);
+    }
+});
+
+function getTotals() {
+    let totalPrice = 0;
+    let totalQuantity = 0;
+
+    const rows = Array.from(itemTable.querySelectorAll('tr')).slice(1, -1);
+    rows.forEach(row => {
+        const quantity = parseInt(row.cells[2].textContent);
+        const price = parseFloat(row.cells[1].textContent.replace('$', ''));
+
+        totalQuantity += quantity;
+        totalPrice += price * quantity;
+    });
+
+    totalQuantityCell.textContent = totalQuantity;
+    totalPriceCell.textContent = "$" + totalPrice;
+}
