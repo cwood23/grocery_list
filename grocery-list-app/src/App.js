74115870e117c9709ce5120e66d55071140030e8
cameron wood <cwood14@huskers.unl.edu>
2024-04-30 19:56:02 -0500

    Functionality replicated

--- grocery-list-app/src/App.js
+++ grocery-list-app/src/App.js
@@ -10,9 +10,32 @@ function App() {
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalQuantity, setTotalQuantity] = useState(0);
 
-  const addRow = () => {
+  const addRow = (e) => {
+    e.preventDefault();
     const newRow = { id: rows.length + 1, name: itemName, price: itemPrice, quantity: itemQuantity};
     setRows([...rows, newRow]);
+
+    setTotalPrice(parseFloat(totalPrice) + (parseFloat(itemPrice) * parseInt(itemQuantity)));
+    setTotalQuantity(parseInt(totalQuantity) + parseInt(itemQuantity));
+  }
+
+  const removeRow = (rowId, rowPrice, rowQuantity) => {
+    setRows(rows.filter(row => row.id !== rowId));
+
+    setTotalPrice(parseFloat(totalPrice) - (parseFloat(rowPrice) * parseInt(rowQuantity)));
+    setTotalQuantity(parseInt(totalQuantity) - parseInt(rowQuantity));
+  }
+
+  const itemNameHandler = (e) => {
+    setItemName(e.target.value);
+  }
+
+  const itemPriceHandler = (e) => {
+    setItemPrice(e.target.value);
+  }
+
+  const itemQuantityHandler = (e) => {
+    setItemQuantity(e.target.value);
   }
 
   return (
@@ -34,17 +57,17 @@ function App() {
           {rows.map(row => (
             <tr key={row.id}>
               <td>{row.name}</td>
-              <td>{row.price}</td>
+              <td>${row.price}</td>
               <td>{row.quantity}</td>
-              <td>{row.price * row.quantity}</td>
-              <td><button className='delete-btn'>X</button></td>
+              <td>${row.price * row.quantity}</td>
+              <td><button className='delete-btn' onClick={() => removeRow(row.id, row.price, row.quantity)}>X</button></td>
             </tr>
           ))}
           <tr>
             <td>Total</td>
             <td></td>
-            <td id='totalQuantity'>0</td>
-            <td id='totalPrice'>0</td>
+            <td id='totalQuantity'>{totalQuantity}</td>
+            <td id='totalPrice'>${totalPrice}</td>
             <td></td>
           </tr>
         </tbody>
@@ -56,15 +79,15 @@ function App() {
         <div className='inputs-div'>
           <div className='input-div'>
             <label className='input-label'>Name: </label>
-            <input className='input-box' type='text' name='name' id='name' required />
+            <input className='input-box' type='text' name='name' id='name' required onChange={itemNameHandler} />
           </div>
           <div className='input-div'>
             <label className='input-label'>($) Price: </label>
-            <input className='input-box' type='number' name='price' id='price' min='0' step='.01' />
+            <input className='input-box' type='number' name='price' id='price' min='0' step='.01' onChange={itemPriceHandler} />
           </div>
           <div className='input-div'>
             <label className='input-label'>Quantity: </label>
-            <input className='input-box' type='number' name='quantity' id='quantity' />
+            <input className='input-box' type='number' name='quantity' id='quantity' onChange={itemQuantityHandler} />
           </div>
         </div>
         <div className='btn-div'>
