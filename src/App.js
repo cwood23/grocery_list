3fe5647bc97f22d56279ca48b73b820d19151c19
cameron wood <cwood14@huskers.unl.edu>
2024-05-02 11:33:09 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,110 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import './App.css';
-import React, { useState } from 'react';
+import Home from "./Components/Home/Home";
 
 function App() {
-  const [rows, setRows] = useState([]);
-
-  const [itemName, setItemName] = useState('');
-  const [itemPrice, setItemPrice] = useState(0);
-  const [itemQuantity, setItemQuantity] = useState(0);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [totalQuantity, setTotalQuantity] = useState(0);
-
-  const addRow = (e) => {
-    e.preventDefault();
-    const newRow = { id: rows.length + 1, name: itemName, price: itemPrice, quantity: itemQuantity};
-    setRows([...rows, newRow]);
-
-    setTotalPrice(parseFloat(totalPrice) + (parseFloat(itemPrice) * parseInt(itemQuantity)));
-    setTotalQuantity(parseInt(totalQuantity) + parseInt(itemQuantity));
-
-    setItemName('');
-    setItemPrice(0);
-    setItemQuantity(0);
-  }
-
-  const removeRow = (rowId, rowPrice, rowQuantity) => {
-    setRows(rows.filter(row => row.id !== rowId));
-
-    setTotalPrice(parseFloat(totalPrice) - (parseFloat(rowPrice) * parseInt(rowQuantity)));
-    setTotalQuantity(parseInt(totalQuantity) - parseInt(rowQuantity));
-  }
-
-  const itemNameHandler = (e) => {
-    setItemName(e.target.value);
-  }
-
-  const itemPriceHandler = (e) => {
-    setItemPrice(e.target.value);
-  }
-
-  const itemQuantityHandler = (e) => {
-    setItemQuantity(e.target.value);
-  }
-
   return (
-    <div>
-      <nav>
-        
-      </nav>
-      <div className='page_header'>
-        <h1 className='header-text'>Grocery List</h1>
-      </div>
-      <table className='list-table' id='itemTable'>
-        <thead>
-          <tr className='list-header-row'>
-            <th className='list-header'>Item</th>
-            <th className='list-header'>Price</th>
-            <th className='list-header'>Qty</th>
-            <th className='list-header'>Total</th>
-            <th className='list-header'></th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map(row => (
-            <tr key={row.id}>
-              <td>{row.name}</td>
-              <td>${row.price}</td>
-              <td>{row.quantity}</td>
-              <td>${row.price * row.quantity}</td>
-              <td><button className='delete-btn' onClick={() => removeRow(row.id, row.price, row.quantity)}>X</button></td>
-            </tr>
-          ))}
-          <tr>
-            <td>Total</td>
-            <td></td>
-            <td id='totalQuantity'>{totalQuantity}</td>
-            <td id='totalPrice'>${totalPrice}</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-
-      <br />
-
-      <form action='' id='newItemForm' className='newItemForm'>
-        <div className='inputs-div'>
-          <div className='input-div'>
-            <label className='input-label'>Name: </label>
-            <input className='input-box' type='text' name='name' id='name' value={itemName} required onChange={itemNameHandler} />
-          </div>
-          <div className='input-div'>
-            <label className='input-label'>($) Price: </label>
-            <input className='input-box' type='number' name='price' id='price' min='0' step='.01' value={itemPrice} onChange={itemPriceHandler} />
-          </div>
-          <div className='input-div'>
-            <label className='input-label'>Quantity: </label>
-            <input className='input-box' type='number' name='quantity' id='quantity' value={itemQuantity} onChange={itemQuantityHandler} />
-          </div>
-        </div>
-        <div className='btn-div'>
-          <button className='add-btn' onClick={addRow}>Add Item</button>
-        </div>
-      </form>
-
-      <p>Get the code <a href='https://github.com/cwood23/grocery_list_app'>here</a></p>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
