932645a580544a60084b3fed22bb4ea3c13f671b
cameron wood <cwood14@huskers.unl.edu>
2024-05-03 17:48:10 -0500

    WIP

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -5,11 +5,48 @@ export const getAllItems = async (req, res) => {
     try {
         const items = await Item.findAll();
         if (items) {
-            res.send(items);
+            res.status(200).send(items);
         } else {
             res.status(404).send("No items found.");
         }
     } catch (err) {
         res.status(500).send("Error finding items: " + err);
+    }
+}
+
+// Get Item by name
+export const getItemByName = async (req, res) => {
+    const { name } = req.query;
+    try {
+        const item = await Item.findOne({ where: { name: name } });
+        if (item) {
+            res.status(200).send(item);
+        } else {
+            res.status(404).send("No item found.");
+        }
+    } catch (err) {
+        res.status(500).send("Error finding item: " + err);
+    }
+}
+
+// Create a new item given name and price
+export const createNewItem = async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    try {
+        const item = await Item.findOne({ where: { name: data.name } });
+        if (item) {
+            res.status(409).send("Item already exists.");
+        } else {
+            const newItem = await Item.create({
+                name: data.name,
+                price: data.price
+            });
+            if (newItem) {
+                res.status(201).send("Item created successfully.");
+            }
+        }
+    } catch (err) {
+        res.status(500).send("Error creating item: " + err);
     }
 }
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllItems } from "./controllers/itemController.js";
+import bodyParser from "body-parser";
+import { createNewItem, getAllItems, getItemByName } from "./controllers/itemController.js";
 
 const router = express.Router();
+var jsonParser = bodyParser.json();
 
 router.get('/getallitems', getAllItems);
+router.get('/getitembyname', getItemByName);
+router.post('/createnewitem', jsonParser, createNewItem);
 
 export default router;
