8370236d63bc5a214646c10c593f43eb7fa21889
cameron wood <cwood14@huskers.unl.edu>
2024-05-03 19:06:26 -0500

    Item add page working

--- server/index.js
+++ server/index.js
@@ -8,7 +8,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Routes here
+// CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Routes
 app.use('/', router);
 
 app.listen(PORT, () => {
--- src/Components/ItemAdd/ItemAdd.js
+++ src/Components/ItemAdd/ItemAdd.js
@@ -1,10 +1,36 @@
-import NavHeader from "../Navbar/NavHeader";
+import NavHeader from '../Navbar/NavHeader';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function ItemAdd() {
     const [itemName, setItemName] = useState('');
     const [itemPrice, setItemPrice] = useState(0);
 
+    const handleNameChange = (e) => {
+        setItemName(e.target.value);
+    }
+
+    const handlePriceChange = (e) => {
+        setItemPrice(e.target.value);
+    }
+
+    const handleCreateItem = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('/createnewitem', {
+                name: itemName,
+                price: itemPrice
+            });
+            console.log(response);
+        } catch (err) {
+            if (err.response.status === 409) {
+                console.log("Item already exists.")
+            } else {
+                console.error("Error creating item.", err);
+            }
+        }
+    }
 
     return (
         <div>
@@ -12,19 +38,19 @@ function ItemAdd() {
             <div className='page_header'>
                 <h1 className='header-text'>Add a New Item to Store</h1>
             </div>
-            <form action='' id='newItemForm' className='newItemForm'>
+            <form id='newItemForm' className='newItemForm'>
                 <div className='inputs-div'>
                     <div className='input-div'>
                         <label className='input-label'>Name: </label>
-                        <input className='input-box' type='text' name='name' id='name' required />
+                        <input className='input-box' type='text' name='name' id='name' required onChange={handleNameChange} />
                     </div>
                     <div className='input-div'>
                         <label className='input-label'>($) Price: </label>
-                        <input className='input-box' type='number' name='price' id='price' min='0' step='.01' />
+                        <input className='input-box' type='number' name='price' id='price' min='0' step='.01' onChange={handlePriceChange} />
                     </div>
                 </div>
                 <div className='btn-div'>
-                    <button className='add-btn'>Add Item to Store</button>
+                    <button className='add-btn' onClick={handleCreateItem}>Add Item to Store</button>
                 </div>
             </form>
         </div>
