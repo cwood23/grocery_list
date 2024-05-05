2379b30914b116abeb1fc5d257bfd9cc7fb5c0ab
cameron wood <cwood14@huskers.unl.edu>
2024-05-05 17:57:01 -0500

    WIP

---
+++ server/controllers/recipeController.js
@@ -0,0 +1,52 @@
+import { Item, Recipe } from '../models.js'
+
+// Get recipe items by name
+export const getRecipeByName = async (req, res) => {
+    try {
+        const { name } = req.query
+        const recipe = await Recipe.findAll({
+            include: [
+                {
+                    model: Item
+                },
+            ],
+            where: {
+                name: name
+            }
+        })
+        if (recipe) {
+            res.status(200).send(recipe);
+        } else {
+            res.status(404).send("No recipe found.");
+        }
+    } catch (err) {
+        res.status(500).send("Error finding recipe.");
+    }
+}
+
+// Create new recipe
+export const createNewRecipe = async (req, res) => {
+    const { name, itemIds } = req.body;
+    try {
+        const recipe = await Recipe.findAll({ where: { name: name } });
+        if (recipe) {
+            res.status(409).send("Recipe already exists.");
+        } else {
+            const newRecipe = await Recipe.create(
+                {
+                    name: name
+                }
+            );
+            if (newRecipe) {
+                await Promise.all(itemIds.map(async (itemId) => {
+                    var item = Item.findByPk(itemId);
+                    await newRecipe.addItem(item);
+                }));
+            }
+            res.status(201).send("Recipe successfully created.");
+        }
+    } catch (err) {
+        console.error("Error creating recipe.", err);
+        res.status(500).send("Error creating recipe.");
+    }
+}
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { createNewItem, getAllItems, getItemByName } from "./controllers/itemController.js";
+import { createNewRecipe, getRecipeByName } from "./controllers/recipeController.js";
 
 const router = express.Router();
 var jsonParser = bodyParser.json();
@@ -8,5 +9,8 @@ var jsonParser = bodyParser.json();
 router.get('/getallitems', getAllItems);
 router.get('/getitembyname', getItemByName);
 router.post('/createnewitem', jsonParser, createNewItem);
+
+router.get('/getrecipebyname', getRecipeByName);
+router.post('/createnewrecipe', jsonParser, createNewRecipe);
 
 export default router;
