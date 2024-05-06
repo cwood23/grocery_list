9fd57eb764f6b1cc2c04e78131c36500eb974360
cwood23 <89489592+cwood23@users.noreply.github.com>
2024-05-06 14:11:56 -0500

    WIP

--- src/Components/RecipeAdd/RecipeAdd.js
+++ src/Components/RecipeAdd/RecipeAdd.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 function RecipeAdd() {
     const [recipeName, setRecipeName] = useState('');
     const [itemName, setItemName] = useState('');
+    const [itemList, setItemList] = useState(['Ham', 'Bacon']);
 
     const handleNameChange = (e) => {
         setRecipeName(e.target.value);
@@ -14,6 +15,7 @@ function RecipeAdd() {
         e.preventDefault();
         console.log(recipeName);
         setRecipeName('');
+        setItemName('');
     }
 
     return (
@@ -22,11 +24,17 @@ function RecipeAdd() {
             <div className='page_header'>
                 <h1 className='header-text'>Create a New Recipe</h1>
             </div>
+            <div>
+                {itemList.map(item => (
+                    <h2>{item}</h2>
+                ))}
+            </div>
             <form id='newItemForm' className='newItemForm'>
                 <div className='inputs-div'>
                     <div className='input-div'>
-                        <label className='input-label'>Name: </label>
+                        <label className='input-label'>Recipe Name: </label>
                         <input className='input-box' type='text' name='name' id='name' value={recipeName} required onChange={handleNameChange} />
+                        <button className='delete-btn'>+</button>
                     </div>
                 </div>
                 <div className='btn-div'>
