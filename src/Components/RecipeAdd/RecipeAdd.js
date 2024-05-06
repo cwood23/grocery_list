9c4d15e3795392e1e6b58a62f39002a3dd26ab5c
cwood23 <89489592+cwood23@users.noreply.github.com>
2024-05-06 14:01:03 -0500

    WIP

--- src/Components/RecipeAdd/RecipeAdd.js
+++ src/Components/RecipeAdd/RecipeAdd.js
@@ -1,12 +1,40 @@
+import React, { useState } from 'react';
 import NavHeader from "../Navbar/NavHeader";
+import axios from 'axios';
 
 function RecipeAdd() {
-    return(
+    const [recipeName, setRecipeName] = useState('');
+    const [itemName, setItemName] = useState('');
+
+    const handleNameChange = (e) => {
+        setRecipeName(e.target.value);
+    }
+
+    const handleCreateRecipe = (e) => {
+        e.preventDefault();
+        console.log(recipeName);
+        setRecipeName('');
+    }
+
+    return (
         <div>
             <NavHeader />
+            <div className='page_header'>
+                <h1 className='header-text'>Create a New Recipe</h1>
+            </div>
+            <form id='newItemForm' className='newItemForm'>
+                <div className='inputs-div'>
+                    <div className='input-div'>
+                        <label className='input-label'>Name: </label>
+                        <input className='input-box' type='text' name='name' id='name' value={recipeName} required onChange={handleNameChange} />
+                    </div>
+                </div>
+                <div className='btn-div'>
+                    <button className='add-btn' onClick={handleCreateRecipe}>Create Recipe</button>
+                </div>
+            </form>
         </div>
     )
-
 }
 
 export default RecipeAdd;
